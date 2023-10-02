9d1e8efea916d99aaabc196c119dbc6fce29f081
caitlinnay <81677625+caitlinnaylor@users.noreply.github.com>
2023-10-02 14:52:00 +1300

    Added responsiveness and some javascript
    text areas change size and become editable and uneditable

--- myscripts.js
+++ myscripts.js
@@ -27,3 +27,56 @@ window.addEventListener('scroll', toggleTableVisibility);
 
 // Initial check when the page loads
 window.addEventListener('load', toggleTableVisibility);
+
+//Edit button for text areas 
+function toggleAboutMeEditable() {
+    const editableTextarea = document.getElementById('about_me');
+
+    if (editableTextarea.getAttribute('readonly') === null) {
+        editableTextarea.setAttribute('readonly', 'readonly');
+        document.getElementById('edit2').textContent = 'EDIT';
+    } else {
+        editableTextarea.removeAttribute('readonly');
+        editableTextarea.focus();
+        document.getElementById('edit2').textContent = 'SAVE';
+    }
+}
+
+function toggleNameAgeEditable() {
+    const editableTextarea = document.getElementById('name_age');
+
+    if (editableTextarea.getAttribute('readonly') === null) {
+        editableTextarea.setAttribute('readonly', 'readonly');
+        document.getElementById('edit1').textContent = 'EDIT';
+    } else {
+        editableTextarea.removeAttribute('readonly');
+        editableTextarea.focus();
+        document.getElementById('edit1').textContent = 'SAVE';
+    }
+}
+
+function toggleThoughtEditable() {
+    const editableTextarea = document.getElementById('thought');
+	alert("hi");
+
+    if (editableTextarea.getAttribute('readonly') === null) {
+        editableTextarea.setAttribute('readonly', 'readonly');
+        document.getElementById('edit3').textContent = 'EDIT';
+    } else {
+        editableTextarea.removeAttribute('readonly');
+        editableTextarea.focus();
+        document.getElementById('edit3').textContent = 'SAVE';
+    }
+}
+
+function toggleKeyInfoEditable(keyinfo) {
+    const editableTextarea = document.getElementById(keyinfo);
+
+    if (editableTextarea.getAttribute('readonly') === null) {
+        editableTextarea.setAttribute('readonly', 'readonly');
+    } else {
+        editableTextarea.removeAttribute('readonly');
+        editableTextarea.focus();
+    }
+}
+
